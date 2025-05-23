e1bad9d1bc731824dad269f0509b927fda8d40f7
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-05-23 16:13:19 +0500

    groups permissions created

---
+++ groups/permissions.py
@@ -0,0 +1,23 @@
+from rest_framework import permissions
+
+class IsGroupLeaderOrReadOnly(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return request.user and request.user.is_authenticated
+
+        return request.user and request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        user = request.user
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        if hasattr(obj, 'members'):
+            return obj.members.filter(user=user, role='leader', is_active=True).exists()
+
+        if hasattr(obj, 'group'):
+            return obj.group.members.filter(user=user, role='leader', is_active=True).exists()
+
+        return False
--- groups/views.py
+++ groups/views.py
@@ -1,12 +1,14 @@
-from rest_framework import viewsets, permissions, filters
+from rest_framework import viewsets, filters
 from .models import Group, Member
 from .serializers import GroupSerializer, MemberSerializer
 from .pagination import GroupPagination, MemberPagination
+from .permissions import IsGroupLeaderOrReadOnly
+
 
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsGroupLeaderOrReadOnly]
     pagination_class = GroupPagination
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['name', 'description', 'institution']
@@ -15,7 +17,7 @@ class GroupViewSet(viewsets.ModelViewSet):
 class MemberViewSet(viewsets.ModelViewSet):
     queryset = Member.objects.all()
     serializer_class = MemberSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsGroupLeaderOrReadOnly]
     pagination_class = MemberPagination
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['user__full_name', 'role']
